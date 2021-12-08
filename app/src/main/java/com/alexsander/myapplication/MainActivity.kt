9785b27c10a6c5b18638a0be766c2a5c685e5882
Alexander <94855033+Alexander505050@users.noreply.github.com>
2021-12-08 20:30:40 +0500

    fhree commit”

--- app/src/main/java/com/alexsander/myapplication/MainActivity.kt
+++ app/src/main/java/com/alexsander/myapplication/MainActivity.kt
@@ -2,11 +2,52 @@ package com.alexsander.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.alexander.R
+const val TAG= "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCrete")
     }
-}
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "Ты видел деву на скале\n" +
+                "В одежде белой над волнами")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "Когда, бушуя в бурной мгле,\n" +
+                "Играло море с берегами,")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "Когда луч молний озарял,\n" +
+                "Ее всечасно блеском алым,")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "И ветер бился и летал\n" +
+                "С ее летучим покрывалом?")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "Прекрасно море в бурной мгле\n" +
+                "И небо в блесках без лазури;")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "Но верь мне: дева на скале\n" +
+                "В одежде белой над волнами")
+    }
+
+
+    }
